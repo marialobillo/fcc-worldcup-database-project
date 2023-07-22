13d249a503d5dd6168fac02f299ad63e05fcf4d4
Maria Lobillo-Santos <maria.lobillo.santos@gmail.com>
2023-07-22 16:44:12 +0200

    Create queries.sh

---
+++ queries.sh
@@ -0,0 +1,39 @@
+#! /bin/bash
+
+PSQL="psql --username=freecodecamp --dbname=worldcup --no-align --tuples-only -c"
+
+echo -e "\nTotal number of goals in all games from winning teams:"
+echo "$($PSQL "SELECT SUM(winner_goals) FROM games")"
+
+echo -e "\nTotal number of goals in all games from both teams combined:"
+echo "$($PSQL "SELECT SUM(winner_goals + opponent_goals) FROM games")"
+
+echo -e "\nAverage number of goals in all games from the winning teams:"
+echo "$($PSQL "SELECT avg(winner_goals) FROM games")"
+
+echo -e "\nAverage number of goals in all games from the winning teams rounded to two decimal places:"
+echo "$($PSQL "SELECT round(avg(winner_goals), 2) FROM games")"
+
+echo -e "\nAverage number of goals in all games from both teams:"
+echo "$($PSQL "SELECT avg(winner_goals + opponent_goals) FROM games")"
+
+echo -e "\nMost goals scored in a single game by one team:"
+echo "$($PSQL "SELECT max(winner_goals) FROM games")"
+
+echo -e "\nNumber of games where the winning team scored more than two goals:"
+echo "$($PSQL "SELECT count(*) from games where winner_goals > 2")"
+
+echo -e "\nWinner of the 2018 tournament team name:"
+echo "$($PSQL "SELECT name from games inner join teams on games.winner_id = teams.team_id where year=2018 and round='Final' order by winner_goals")"
+
+echo -e "\nList of teams who played in the 2014 'Eighth-Final' round:"
+echo "$($PSQL "SELECT name from teams left join games on teams.team_id = games.winner_id or teams.team_id = games.opponent_id where year=2014 and round='Eighth-Final' order by name")"
+
+echo -e "\nList of unique winning team names in the whole data set:"
+echo "$($PSQL "SELECT distinct(name) from teams inner join games on teams.team_id = games.winner_id order by name")"
+
+echo -e "\nYear and team name of all the champions:"
+echo "$($PSQL "SELECT year, name from teams inner join games on teams.team_id = games.winner_id where round='Final' order by year")"
+
+echo -e "\nList of teams that start with 'Co':"
+echo "$($PSQL "SELECT name from teams where name like 'Co%'")"
